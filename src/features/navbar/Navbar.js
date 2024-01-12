24d51a930328b9dc28dd3ec9eb6485fa68b9e047
Ashish <bansalgashi083@gmail.com>
2024-01-12 23:44:58 +0530

    redux setup

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -39,11 +39,13 @@ const Navbar = ({ children }) => {
               <div className="flex h-16 items-center justify-between">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                      alt="Your Company"
-                    />
+                    <Link to="/">
+                      <img
+                        className="h-8 w-8"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        alt="Your Company"
+                      />
+                    </Link>
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
@@ -68,20 +70,19 @@ const Navbar = ({ children }) => {
                 <div className="hidden md:block">
                   <div className="ml-4 flex items-center md:ml-6">
                     <Link to="/Cart">
-                    <button
-                      type="button"
-                      className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="absolute -inset-1.5" />                    
-                      
-                      <ShoppingCartIcon
-                        className="h-6 w-6"
-                        aria-hidden="true"
-                      />
-                      
-                    </button>
+                      <button
+                        type="button"
+                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="absolute -inset-1.5" />
+
+                        <ShoppingCartIcon
+                          className="h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      </button>
                     </Link>
-                    
+
                     <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 mb-4     text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                       3
                     </span>
@@ -181,19 +182,21 @@ const Navbar = ({ children }) => {
                     </div>
                   </div>
                   <Link to="/Cart">
-                  <button
-                    type="button"
-                    className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="absolute -inset-1.5" />                    
-                    <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-                    
-                  </button>
+                    <button
+                      type="button"
+                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <ShoppingCartIcon
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      />
+                    </button>
                   </Link>
-                  
+
                   <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 mb-4     text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                      3
-                    </span>
+                    3
+                  </span>
                 </div>
                 <div className="mt-3 space-y-1 px-2">
                   {userNavigation.map((item) => (
--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,6 +1,7 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080')
+    //ToDo: we will nmot hard code the server url here
+    const response = await fetch('http://localhost:8080/products')
     const data =  await response.json()
     resolve({data})}
   );
