463b0abccf2e48e7ec52958a2fda55cfb201e6b4
Ashish <bansalgashi083@gmail.com>
2024-03-20 00:02:16 +0530

    minor changes

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,44 +1,37 @@
 export function createUser(userData) {
-  return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080/auth/signup',
-    {
-      method:'POST',
-      body:JSON.stringify(userData),
-      headers:{"content-type":'aplication/json'}
-
-    })
-    const data =  await response.json()
-    resolve({data})}
-  );
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/auth/signup", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: { "content-type": "aplication/json" },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
 }
 export function checkUser(loginInfo) {
-  return new Promise(async (resolve,reject) =>{
-    const email = loginInfo.email
-    const password = loginInfo.password
-    const response = await fetch('http://localhost:8080/users?email='+email)
-    const data =  await response.json()
-    console.log(data)
-    if(data.length){
-      if(password===data[0].password){
-        resolve({data:data[0]})
-      }
-      else{
-        reject({message:"wrong credential"})        
-      }
-      
+  return new Promise(async (resolve, reject) => {
+    try {
+      const email = loginInfo.email;
+      const password = loginInfo.password;
+      const response = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        body: JSON.stringify(loginInfo),
+        headers: { "content-type": "aplication/json" },
+      });
+      const data = await response.json();
+      console.log(data);
+
+      resolve({ data });
+    } catch (err) {
+      reject({ err });
     }
-    else{
-      reject({message:"user not found"})
-    }
-    }
-    
-  );
+  });
 }
 
 export function signOut(userId) {
-  return new Promise(async (resolve) =>{
+  return new Promise(async (resolve) => {
     // Todo: on server we will remove user session info
-    resolve({data:'success'})}
-  );
+    resolve({ data: "success" });
+  });
 }
-
